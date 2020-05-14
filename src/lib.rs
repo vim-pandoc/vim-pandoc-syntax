2557a7a050c514e11c01665d86d88818fa1df79f
Caleb Maclennan <caleb@alerque.com>
2020-05-15 02:47:42 +0300

    Add basic functions to test module build

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+#[macro_use]
+extern crate mlua_derive;
+
+use mlua::prelude::*;
+use pulldown_cmark::{Options, Parser};
+
+fn hello(_: &Lua, name: String) -> LuaResult<()> {
+    println!("hello, {}!", name);
+    Ok(())
+}
+
+#[lua_module]
+fn my_module(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("hello", lua.create_function(hello)?)?;
+    Ok(exports)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
