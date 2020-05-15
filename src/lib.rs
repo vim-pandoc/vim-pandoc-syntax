ae32d682248da97c1ae97fa99d03f6c84a503f04
Caleb Maclennan <caleb@alerque.com>
2020-05-15 18:15:10 +0300

    Render and return HTML for passed Markdown

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,20 @@
 extern crate mlua_derive;
 
 use mlua::prelude::*;
+use pulldown_cmark::{html, Options, Parser};
 
-fn hello(_: &Lua, name: String) -> LuaResult<String> {
-    let a: String = String::from(format!("not you {} he he", name));
-    Ok(a)
+fn render_html(_: &Lua, buffer: String) -> LuaResult<String> {
+    let options = Options::all();
+    let parser = Parser::new_ext(buffer.as_str(), options);
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    Ok(html_output)
 }
 
 #[lua_module]
 fn libvim_pandoc_syntax(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
-    exports.set("hello", lua.create_function(hello)?)?;
+    exports.set("render_html", lua.create_function(render_html)?)?;
     Ok(exports)
 }
 
