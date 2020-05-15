ec6e40690d5f6fa6dd0858cddd30d973a3158589
Caleb Maclennan <caleb@alerque.com>
2020-05-15 18:59:40 +0300

    Add function that returns Lua table of tag offsets

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 extern crate mlua_derive;
 
 use mlua::prelude::*;
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, Event, Options, Parser};
 
 fn render_html(_: &Lua, buffer: String) -> LuaResult<String> {
     let options = Options::all();
@@ -12,10 +12,25 @@ fn render_html(_: &Lua, buffer: String) -> LuaResult<String> {
     Ok(html_output)
 }
 
+fn get_offsets(lua: &Lua, buffer: String) -> LuaResult<LuaTable> {
+    let options = Options::all();
+    let parser = Parser::new_ext(buffer.as_str(), options);
+    let starts = lua.create_table()?;
+    let mut i: u32 = 1;
+    for (event, range) in parser.into_offset_iter() {
+        if let Event::Start(tag) = event {
+            starts.set(i, format!("event at {:?} = {:#?}", range, tag))?;
+            i += 1;
+        }
+    }
+    Ok(starts)
+}
+
 #[lua_module]
 fn libvim_pandoc_syntax(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("render_html", lua.create_function(render_html)?)?;
+    exports.set("get_offsets", lua.create_function(get_offsets)?)?;
     Ok(exports)
 }
 
