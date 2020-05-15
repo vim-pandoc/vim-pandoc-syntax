e1f6e5d9fb98e65c7d9f4e23a27fcf3d49be6871
Caleb Maclennan <caleb@alerque.com>
2020-05-15 18:06:48 +0300

    Round trip a string from Neovim -> Rust -> Neovim

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,14 @@
 extern crate mlua_derive;
 
 use mlua::prelude::*;
-use pulldown_cmark::{Options, Parser};
 
-fn hello(_: &Lua, name: String) -> LuaResult<()> {
-    println!("hello, {}!", name);
-    Ok(())
+fn hello(_: &Lua, name: String) -> LuaResult<String> {
+    let a: String = String::from(format!("not you {} he he", name));
+    Ok(a)
 }
 
 #[lua_module]
-fn my_module(lua: &Lua) -> LuaResult<LuaTable> {
+fn libvim_pandoc_syntax(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("hello", lua.create_function(hello)?)?;
     Ok(exports)
