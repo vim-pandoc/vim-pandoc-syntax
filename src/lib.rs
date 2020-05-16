61e0679de7b770169f593aecd52efce8c4458625
Caleb Maclennan <caleb@alerque.com>
2020-05-16 19:36:22 +0300

    Bring up both to_html() and get_offsets() in both modules

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,6 @@ pub mod python;
 use pulldown_cmark::{html, Event, Options, Parser, Tag};
 use std::{error, result};
 
-// #[cfg(feature = "lua")]
-// type LibResult<T> = lua::LibResult<T>;
-
-// #[cfg(feature = "python")]
-// type LibResult<T> = python::LibResult<T>;
-
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
 fn to_html(buffer: String) -> Result<String> {
@@ -23,27 +17,32 @@ fn to_html(buffer: String) -> Result<String> {
     Ok(html_output)
 }
 
-// fn get_event_offsets(&Lua, buffer: String) -> LibResult<LuaTable> {
-//     let options = Options::all();
-//     let parser = Parser::new_ext(buffer.as_str(), options);
-//     let events = lua.create_table()?;
-//     let mut i: u32 = 1;
-//     for (event, range) in parser.into_offset_iter() {
-//         if let Event::Start(tag) = event {
-//             let group = match tag {
-//                 Tag::Heading(n) => format!("cmarkHeading{}", n),
-//                 _ => format!("cmark{:?}", tag),
-//             };
-//             let info = lua.create_table()?;
-//             info.set("group", group)?;
-//             info.set("first", range.start + 1)?;
-//             info.set("length", range.end - range.start)?;
-//             events.set(i, info)?;
-//             i += 1;
-//         }
-//     }
-//     Ok(events)
-// }
+#[derive(Debug)]
+struct MdTag {
+    group: String,
+    first: usize,
+    last: usize,
+}
+
+type Events = Vec<MdTag>;
+
+fn get_offsets(buffer: String) -> Result<Events> {
+    let options = Options::all();
+    let parser = Parser::new_ext(buffer.as_str(), options);
+    let mut events = Events::new();
+    for (event, range) in parser.into_offset_iter() {
+        if let Event::Start(tag) = event {
+            let group: String = match tag {
+                Tag::Heading(n) => format!("cmarkHeading{}", n),
+                _ => format!("cmark{:?}", tag),
+            };
+            let first = range.start + 1;
+            let last = range.end + 1;
+            events.push(MdTag { group, first, last });
+        }
+    }
+    Ok(events)
+}
 
 #[cfg(test)]
 mod tests {
--- src/lua.rs
+++ src/lua.rs
@@ -5,10 +5,23 @@ fn to_html(_: &Lua, buffer: String) -> LuaResult<String> {
     Ok(super::to_html(buffer).unwrap())
 }
 
+fn get_offsets(lua: &Lua, buffer: String) -> LuaResult<LuaTable> {
+    let table = lua.create_table().unwrap();
+    let events = super::get_offsets(buffer).unwrap();
+    for (i, event) in events.iter().enumerate() {
+        let info = lua.create_table().unwrap();
+        info.set("group", event.group.as_str()).unwrap();
+        info.set("first", event.first).unwrap();
+        info.set("last", event.last).unwrap();
+        table.set(i, info).unwrap();
+    }
+    Ok(table)
+}
+
 #[lua_module]
 fn libvim_pandoc_syntax(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
     exports.set("to_html", lua.create_function(to_html)?)?;
-    // exports.set("get_event_offsets", lua.create_function(get_event_offsets)?)?;
+    exports.set("get_offsets", lua.create_function(get_offsets)?)?;
     Ok(exports)
 }
